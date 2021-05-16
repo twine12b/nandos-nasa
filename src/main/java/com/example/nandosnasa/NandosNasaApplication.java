174c8907730a7a2fb838cc59985f3df6f69a745b
aczx131 <richard.renaud@city.co.uk>
2021-05-16 11:42:35 +0100

    Initial Spring sandbox project

---
+++ src/main/java/com/example/nandosnasa/NandosNasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nandosnasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NandosNasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NandosNasaApplication.class, args);
+    }
+
+}
